245a18128d4eaa0a5988ec422cac6860001b599e
hailey <hyf16@ic.ac.uk>
2018-10-25 00:43:39 +0100

    Added contrcutor to TcrConnection class

--- simulator/TcrConnection.js
+++ simulator/TcrConnection.js
@@ -1,7 +1,20 @@
+const Web3 = require('web3');
+
 export default class TcrConnection {
-  submit(initiator, submissionQuality) { // eslint-disable-line
+  contructor(provider, contractAddr, contractAbi) {
+    this.web3 = new Web3(provider);
+    this.contract = this.web3.eth.contract(contractAbi).at(contractAddr);
   }
 
-  vote(initiator, submission, accept) { // eslint-disable-line
+  submit(initiator, submissionQuality) {
+    this.contract.submit(initiator, submissionQuality).call();
+  }
+
+  vote(initiator, submission, accept) {
+    this.contract.vote(initiator, submission, accept).call();
+  }
+
+  getPendingList() {
+    this.contract.getPendingList().call();
   }
 }
