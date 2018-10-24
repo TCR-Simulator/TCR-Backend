254b2ac776f0a7641fcab309aaa6fc9908b3c4ba
Noel Lee <leekayau@gmail.com>
2018-10-24 22:49:43 +0100

    Implemented basic addAgentGroup function in Simulation

--- simulator/Simulation.js
+++ simulator/Simulation.js
@@ -1,3 +1,5 @@
+import { AgentType, Maintainer, Contributor, User } from './Agent';
+
 export default class Simulation {
   constructor() {
     this.agents = [];
@@ -11,5 +13,25 @@ export default class Simulation {
     // create TCR connection
 
     return new Promise(resolve => setTimeout(resolve, 100));
+  }
+
+  addAgentGroup(agentType, behaviors, count) { // eslint-disable-line
+    for (let i = 0; i < count; i++) {
+      let agent;
+      switch (agentType) {
+        case AgentType.MAINTAINER:
+          agent = new Maintainer();
+          break;
+        case AgentType.CONTRIBUTOR:
+          agent = new Contributor();
+          break;
+        case AgentType.USER:
+          agent = new User();
+          break;
+        default:
+          throw TypeError(`Unrecognized agent type "${agentType}"`);
+      }
+      this.agents.push(agent);
+    }
   }
 }
